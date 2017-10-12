3e74baf615605db1fd9b70d86e99ede5c4ee3da5
jguidoux <jeremie.guidoux@tocea.com>
2017-10-12 17:17:48 +0200

    Ajout du mapping Get pour /'url /hello

--- src/main/java/fr/slickteam/decouverte/guildwar/controller/HomeController.java
+++ src/main/java/fr/slickteam/decouverte/guildwar/controller/HomeController.java
@@ -1,12 +1,14 @@
 package fr.slickteam.decouverte.guildwar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
 
-    public String home() {
+    @GetMapping("/hello")
+    public String hello() {
         return "Hello World";
     }
 }
