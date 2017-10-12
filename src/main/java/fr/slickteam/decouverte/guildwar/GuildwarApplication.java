6b0f89ab5ffaf77f3898391df7ad203e9db5af3e
jguidoux <jeremie.guidoux@tocea.com>
2017-10-12 16:05:52 +0200

    initialisation of the broject

---
+++ src/main/java/fr/slickteam/decouverte/guildwar/GuildwarApplication.java
@@ -0,0 +1,12 @@
+package fr.slickteam.decouverte.guildwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildwarApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/slickteam/decouverte/guildwar/GuildwarApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.slickteam.decouverte.guildwar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuildwarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
