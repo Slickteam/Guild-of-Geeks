1aba8054ed137a7e67988bfe54dfe7d76be896ac
jguidoux <jeremie.guidoux@tocea.com>
2017-10-12 17:15:15 +0200

    Ajout de la méthode home

--- src/main/java/fr/slickteam/decouverte/guildwar/controller/HomeController.java
+++ src/main/java/fr/slickteam/decouverte/guildwar/controller/HomeController.java
@@ -4,4 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+
+    public String home() {
+        return "Hello World";
+    }
 }
