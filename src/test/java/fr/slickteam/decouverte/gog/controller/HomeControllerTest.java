483463978596aa9f1761da5b908dca0e65d08adc
jguidoux <jeremie.guidoux@tocea.com>
2018-04-02 16:55:42 +0200

    add integration test

--- src/test/java/fr/slickteam/decouverte/gog/controller/HomeControllerTest.java
+++ src/test/java/fr/slickteam/decouverte/gog/controller/HomeControllerTest.java
@@ -1,4 +1,24 @@
 package fr.slickteam.decouverte.gog.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HomeControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void return_helloworld_wher_get_hello() {
+        String body = this.restTemplate.getForObject("/hello", String.class);
+        assertThat(body).isEqualTo("Hello World");
+    }
 }
