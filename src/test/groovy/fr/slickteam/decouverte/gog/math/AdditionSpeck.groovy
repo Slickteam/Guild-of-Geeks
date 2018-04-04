a95670eebc5870b90b2128d22dc3277d9eec65fe
jguidoux <jeremie.guidoux@tocea.com>
2018-04-04 23:53:26 +0200

    add spock keywords given, when, then

--- src/test/groovy/fr/slickteam/decouverte/gog/math/AdditionSpeck.groovy
+++ src/test/groovy/fr/slickteam/decouverte/gog/math/AdditionSpeck.groovy
@@ -6,5 +6,10 @@ import spock.lang.Specification
 class AdditionSpeck extends Specification {
 
     def "check that 5 + 2 is equal to 7"(){
+        given:
+
+        when:
+
+        then:
     }
 }
